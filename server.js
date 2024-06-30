badfc2b00c8e81d31b5eb5463c0fdd68065a96c4
Ravinodesousa <Ravinodesousa@gmail.com>
2024-06-30 12:26:23 +0100

    included code to fix cors errors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const musicRoute = require("./routes/musicRoute");
 
 // Define a port number
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(musicRoute);
